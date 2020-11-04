33f348cdb25c40a999939a90d8fbdc07f6445b4e
Restyled.io <commits@restyled.io>
2020-11-04 21:46:29 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
  */
 // ANCHOR Show/Close DOM selectors
-const rulesBtn = document.getElementById('rules-btn')
-const closeBtn = document.getElementById('close-btn')
-const rules = document.getElementById('rules')
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
 
 // ANCHOR Canvas DOM selectors
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 // TODO
 /**
@@ -28,67 +28,66 @@ const ctx = canvas.getContext('2d');
 13. Lose - redraw bricks, reset score
  */
 
-let score = 0 // Setting our score board to zero
+let score = 0; // Setting our score board to zero
 
-const brickRowCount = 9;    // Setting 9 bricks on a row
+const brickRowCount = 9; // Setting 9 bricks on a row
 const brickColumnCount = 5; // Setting 5 bricks on column
 
 // Create ball props
 const ball = {
-  x : canvas.width / 2,  // Start in the middle
-  y : canvas.height / 2, // Start in the middle
-  size : 10,             // ball
-  speed : 4,             // Animation  speed prop
-  dx : 4,                // Animation direction
-  dy : -4                // Animation direction with - so it does not move down
-}
+  x: canvas.width / 2, // Start in the middle
+  y: canvas.height / 2, // Start in the middle
+  size: 10, // ball
+  speed: 4, // Animation  speed prop
+  dx: 4, // Animation direction
+  dy: -4, // Animation direction with - so it does not move down
+};
 
 // Create paddle props
 const paddle = {
-  x : canvas.width / 2 - 40, // We are taking half width of the paddle
-  y : canvas.height - 20,    // Center in the middle
-  w : 80,
-  h : 10,
-  speed : 8,
-  dx : 0 // Only moving on the x-axes
+  x: canvas.width / 2 - 40, // We are taking half width of the paddle
+  y: canvas.height - 20, // Center in the middle
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0, // Only moving on the x-axes
 };
 
 // Create brick props
 const brickInfo = {
-  w : 70, // bricks sharing same props
-  h : 20,
-  padding : 10,
-  offsetX : 45,  // position on the x-axes
-  offsetY : 60,  // position on the y-axes
-  visible : true // When hit the brick it will be removed
+  w: 70, // bricks sharing same props
+  h: 20,
+  padding: 10,
+  offsetX: 45, // position on the x-axes
+  offsetY: 60, // position on the y-axes
+  visible: true, // When hit the brick it will be removed
 };
 
 // Create bricks
-const bricks = [];                        // init bricks array
-for (let i = 0; i < brickRowCount; i++) { // loop through array row
+const bricks = []; // init bricks array
+for (let i = 0; i < brickRowCount; i++) {
+  // loop through array row
   bricks[i] = []; //  Set the row bricks array iteration to an empty array
-  for (let j = 0; j < brickColumnCount; j++) { // loop through array column
-    const x = i * (brickInfo.w + brickInfo.padding) +
-              brickInfo.offsetX; // i is the row iteration for each brick
-    const y = j * (brickInfo.h + brickInfo.padding) +
-              brickInfo.offsetY; // we are lopping and getting position
+  for (let j = 0; j < brickColumnCount; j++) {
+    // loop through array column
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX; // i is the row iteration for each brick
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY; // we are lopping and getting position
     bricks[i][j] = {
       x,
       y,
-      ...brickInfo
+      ...brickInfo,
     }; // copy and take the array 2D and give it the values of x,y with the
-       // object values
+    // object values
   }
 }
 
-console.log(bricks)
+console.log(bricks);
 
 // Draw ball on canvas - check MDN canvas drawing paths
 function drawBall() {
   ctx.beginPath(); // Create a path
-  ctx.arc(ball.x, ball.y, ball.size, 0,
-          Math.PI * 2);      // Draw an arc to build a circle
-  ctx.fillStyle = '#0095dd'; // style the property
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2); // Draw an arc to build a circle
+  ctx.fillStyle = "#0095dd"; // style the property
   ctx.fill();
   ctx.closePath();
 }
@@ -97,25 +96,25 @@ function drawBall() {
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-  ctx.fillStyle = '#0095dd';
+  ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
 }
 
 // Draw the score board
 function drawScore() {
-  ctx.font = '20px Arial';
+  ctx.font = "20px Arial";
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
 // Draw bricks on canvas
 function drawBricks() {
-  bricks.forEach(column => { // For columns
-    column.forEach(brick => {
+  bricks.forEach((column) => {
+    // For columns
+    column.forEach((brick) => {
       ctx.beginPath();
       ctx.rect(brick.x, brick.y, brick.w, brick.h);
-      ctx.fillStyle =
-          brick.visible ? '#0095dd' : 'transparent'; // This will be conditional
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent"; // This will be conditional
       ctx.fill();
       ctx.closePath();
     });
@@ -123,18 +122,19 @@ function drawBricks() {
 }
 
 // REVIEW Move paddle on canvas
-function movePaddle() { // Every time draw  on the canvas with can re-draw with
-                        // certain element
-  paddle.x +=
-      paddle.dx; // paddle will not move until we use the keyboards events
+function movePaddle() {
+  // Every time draw  on the canvas with can re-draw with
+  // certain element
+  paddle.x += paddle.dx; // paddle will not move until we use the keyboards events
 
   // Wall detection
-  if (paddle.x + paddle.w > canvas.width) { // entire width of the canvas
-    paddle.x = canvas.width - paddle.w;     // minus the paddle width
+  if (paddle.x + paddle.w > canvas.width) {
+    // entire width of the canvas
+    paddle.x = canvas.width - paddle.w; // minus the paddle width
   }
 
-  if (paddle.x <
-      0) { // 0 from the x-axes  and this is for the borders detection
+  if (paddle.x < 0) {
+    // 0 from the x-axes  and this is for the borders detection
     paddle.x = 0;
   }
 }
@@ -145,43 +145,45 @@ function moveBall() {
   ball.y += ball.dy; // append the ball on y-axes
 
   // Wall collision (right/left)
-  if (ball.x + ball.size > canvas.width ||
-      ball.x - ball.size < 0) { // right and left walls
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    // right and left walls
     ball.dx *= -1; // ball.dx = ball.dx * -1 the reason we are doing this is to
-                   // reverse the ball to go the other wall and bounce back
+    // reverse the ball to go the other wall and bounce back
   }
 
   // Wall collision (top/bottom)
-  if (ball.y + ball.size > canvas.height ||
-      ball.y - ball.size < 0) { // top and bottom walls
-    ball.dy *=
-        -1; // the reason we are doing this is to reverse the ball to go the
-            // other wall and bounce back and not go outside the canvas
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    // top and bottom walls
+    ball.dy *= -1; // the reason we are doing this is to reverse the ball to go the
+    // other wall and bounce back and not go outside the canvas
   }
 
   // console.log(ball.x, ball.y);
 
   // Paddle collision
-  if (ball.x - ball.size > paddle.x && // Always to in consideration the object
-                                       // size of ball and this check the left
-      ball.x + ball.size < paddle.x + paddle.w && // checking the right side
-      ball.y + ball.size > paddle.y) {
+  if (
+    ball.x - ball.size > paddle.x && // Always to in consideration the object
+    // size of ball and this check the left
+    ball.x + ball.size < paddle.x + paddle.w && // checking the right side
+    ball.y + ball.size > paddle.y
+  ) {
     ball.dy = -ball.speed; // reverse the ball object and bounce off
   }
 
   // Brick collision
-  bricks.forEach(column => { // loop through bricks array
-    column.forEach(brick => {
-      if (brick.visible) { // make sure the bricks are visible
+  bricks.forEach((column) => {
+    // loop through bricks array
+    column.forEach((brick) => {
+      if (brick.visible) {
+        // make sure the bricks are visible
         if (
-            ball.x - ball.size > brick.x &&           // left brick side check
-            ball.x + ball.size < brick.x + brick.w && // right brick side check
-            ball.y + ball.size > brick.y &&           // top brick side check
-            ball.y - ball.size < brick.y + brick.h    // bottom brick side check
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
         ) {
           ball.dy *= -1; // bounce off the brick
-          brick.visible =
-              false; // once bounce off the brick make it visible false
+          brick.visible = false; // once bounce off the brick make it visible false
 
           increaseScore(); // change the status
         }
@@ -190,9 +192,10 @@ function moveBall() {
   });
 
   // Hit bottom wall - Lose
-  if (ball.y + ball.size > canvas.height) { // if hit bottom wall then lose
-    showAllBricks();                        // redraw bricks
-    score = 0;                              // reset score
+  if (ball.y + ball.size > canvas.height) {
+    // if hit bottom wall then lose
+    showAllBricks(); // redraw bricks
+    score = 0; // reset score
   }
 }
 
@@ -200,29 +203,31 @@ function moveBall() {
 function increaseScore() {
   score++; // increment by one
 
-  if (score % (brickColumnCount * brickRowCount) ===
-      0) { // check the if there are no bricks if true redraw the bricks wall.
-           // score of 45 % 45 bricks becomes 0
+  if (score % (brickColumnCount * brickRowCount) === 0) {
+    // check the if there are no bricks if true redraw the bricks wall.
+    // score of 45 % 45 bricks becomes 0
     showAllBricks(); // create bricks wall
   }
 }
 
 // Make all bricks appear
-function showAllBricks() {   // loop through the array of bricks
-  bricks.forEach(column => { // for column set visible to true
-    column.forEach(brick => (brick.visible = true));
+function showAllBricks() {
+  // loop through the array of bricks
+  bricks.forEach((column) => {
+    // for column set visible to true
+    column.forEach((brick) => (brick.visible = true));
   });
 }
 
 // REVIEW Setup all the drawing
 function draw() {
   // Clear the canvas to make sure ends the event once stop using the keys
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawBall()
-  drawPaddle()
-  drawScore()
-  drawBricks()
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
 }
 
 // Update canvas drwaing and animation
@@ -236,16 +241,17 @@ function update() {
   requestAnimationFrame(update);
 }
 
-update() // Calling the update function
+update(); // Calling the update function
 
 // Keydown event
 function keyDown(e) {
   // console.log(1) for any key
   // console.log(e.key) To target the arrow key
-  if (e.key === 'Right' ||
-      e.key === 'ArrowRight') { //  so cases it refers to right
-    paddle.dx = paddle.speed;   // take and set dx to speed paddle
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') { // same for the left
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    //  so cases it refers to right
+    paddle.dx = paddle.speed; // take and set dx to speed paddle
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    // same for the left
     paddle.dx = -paddle.speed;
   }
 }
@@ -254,18 +260,22 @@ function keyDown(e) {
 function keyUp(e) {
   // console.log(2)
   // console.log(e.key) // To target the arrow key
-  if (e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' ||
-      e.key === 'ArrowLeft') {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
     paddle.dx = 0; // Stop the key once its up
   }
 }
 
 // Keyboard event handlers up+down
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // Rules and close event handlers
-rulesBtn.addEventListener('click', () => rules.classList.add('show'))
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 
 // Close button
-closeBtn.addEventListener('click', () => rules.classList.remove('show'))
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
